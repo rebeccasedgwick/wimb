c95ff2ea283c960f3d0729ccad52a7634301d35f
rebeccasedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2019-08-04 10:16:19 +0100

    Show workers' shift log

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -2,5 +2,6 @@ class Worker < ApplicationRecord
   has_many :staffings
   has_one :active_staffing, -> { where(active: true) }, class_name: "Staffing"
   has_many :courts, through: :staffings
+  has_many :shifts, through: :staffings
   validates :name, presence: true
 end
